1665a59e578fdc62d5f3d7b8e6902478ac25a0df
ramu siripalli <ramsiripalli2772@gmail.com>
2026-06-17 15:28:04 +0530

    configure application routing and page navigation structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Jobs from "./pages/Jobs";
+import ResumeAnalyzer from "./pages/ResumeAnalyzer";
+import InterviewPrep from "./pages/InterviewPrep";
+import JobMatch from "./pages/JobMatch";
 
 function App() {
-
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <button className="btn btn-primary">
-        Udyoganetra AI Test
-      </button>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/resume-analyzer" element={<ResumeAnalyzer />} />
+        <Route path="/interview-prep" element={<InterviewPrep />} />
+        <Route path="/job-match" element={<JobMatch />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
