e7b57466aeec5403a77a4de17cc49a905a3332a3
ramu siripalli <ramsiripalli2772@gmail.com>
2026-06-18 14:09:09 +0530

    feat: create backend boilerplate architecture

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+// database connection
+connectDB();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api/auth", authRoutes);
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
